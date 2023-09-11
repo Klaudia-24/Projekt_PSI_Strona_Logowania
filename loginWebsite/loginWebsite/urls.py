64b7070625fabe97a0c7bf53458ecb7b3d7b3fe6
Klaudia-24 <klaudia.winer@gmail.com>
2023-09-11 15:40:31 +0200

    Home page changed to view function.

--- loginWebsite/loginWebsite/urls.py
+++ loginWebsite/loginWebsite/urls.py
@@ -19,8 +19,11 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+
+from .views import homePage
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("accounts.urls")),
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path("", homePage, name="home")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
